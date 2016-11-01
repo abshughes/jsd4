5e94ea8869127486d4dc2792228dec720d2675c5
abshughes <anna.ba.hughes@gmail.com>
2016-10-31 21:22:13 -0700

    giphy is back!

---
+++ giphy/giphy.js
@@ -0,0 +1,64 @@
+var mockData = {
+    pagination: {
+        total_count: 37
+    },
+    data: [{
+        id: "feqkVgjJpYtjy",
+        url: "http://giphy.com/gifs/eyes-shocked-bird-feqkVgjJpYtjy",
+        rating: "g",
+        images: {
+            original: {
+                url: "http://media0.giphy.com/media/feqkVgjJpYtjy/giphy.gif"
+            }
+        }
+    },{
+        id: "FiGiRei2ICzzG",
+        url: "http://giphy.com/gifs/funny-cat-FiGiRei2ICzzG",
+        rating: "g",
+        images: {
+            original: {
+                url: "http://media2.giphy.com/media/FiGiRei2ICzzG/giphy.gif"
+            }
+        }
+    }]
+
+};
+
+//Get Template Source HTML from DOM
+var gifTemplate = document.querySelector('#gif-template');
+
+//compile the Handlebars Tempalte/
+var gifTemplateFn=Handlebars.compile(gifTemplate.innerHTML);
+
+//st3
+var catAsString=gifTemplateFn(mockData);
+
+var newcat=document.querySelector('.newcat');
+newcat.innerHTML=catAsString;
+
+//const mochData={
+//	pagination:{
+//		total_count:37
+//	}, 
+//	data: [{
+//		id:'op7uqYWBm3R04',
+//		url:'http://giphy.com/gifs/food-krush-groove-the-fat-boys-op7uqYWBm3R04',
+//		rating:'y', 
+//		images:{
+//			original:{
+//				url:'http://media0.giphy.com/media/op7uqYWBm3R04/giphy.gif'
+//			}
+//		}
+//	},{
+//		id:'wWAIKcFASEFz2', 
+//		url:"http://giphy.com/gifs/superman-santa-chandler-bing-wWAIKcFASEFz2", 
+//		rating:'g'
+//	}]
+//};
+
+
+
+
+
+
+
